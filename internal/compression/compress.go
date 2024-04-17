4bce183e72c56e1e853159833d74a0cad5df40b4
Kleonikos Kyriakis <nikosgr8@gmail.com>
2024-04-17 12:58:48 +0300

    Decrease max chunk size to safely transmit larger messages (#15)
    Co-authored-by: Kleonikos Kyriakis <kleonikos.kyriakis@prodyna.com>

--- internal/compression/compress.go
+++ internal/compression/compress.go
@@ -14,7 +14,7 @@ const (
 	// MaxChunkSize a moderate/safe max chunk size is 48KB. This is because the maximum size of a matrix event is 64KB.
 	// Megolm encryption adds an extra 33% overhead to the encrypted content due to base64 encryption. This means that
 	// the maximum size of pre-encrypted chunk should be 48KB / 1.33 ~= 36KB. We round down to 35KB to be safe.
-	MaxChunkSize = 35 << 10 // max pre-encrypted chunk size is 35KB
+	MaxChunkSize = 30 << 10 // max pre-encrypted chunk size is 30KB - 35KB proved to be an unsafe limit (TODO investigate optimal limit)
 )
 
 var encoder, _ = zstd.NewWriter(nil)
