060b889c8bdb6eaed4324338a435f03341dd570f
Ekrem Seren <ekrem.seren@gmail.com>
2024-12-04 22:09:59 +0300

    use dirty param

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -8,7 +8,7 @@ current_branch=${current_branch_temp////-}
 
 # camino-messenger-bot and caminoethvm git tag and sha
 git_commit=${CAMINO_BOT_COMMIT:-$(git rev-parse --short HEAD)}
-git_tag=${CAMINO_BOT_TAG:-$(git describe --tags --abbrev=0 --always || echo unknown)}
+git_tag=${CAMINO_BOT_TAG:-$(git describe --tags --always --dirty || echo unknown)}
 
 # get protocol releases from buf.build
 grpc_release=$(scripts/resolve_protocol_release.sh buf.build/gen/go/chain4travel/camino-messenger-protocol/grpc/go)
