437a27de87fd586d8aa4344da98992cb8b5cbecb
evlekht <ev.lekht@gmail.com>
2024-12-17 08:45:33 +0400

    Update pkg/booking/booking.go
    Co-authored-by: Ekrem Seren <ekrem.seren@gmail.com>

--- pkg/booking/booking.go
+++ pkg/booking/booking.go
@@ -21,9 +21,12 @@ import (
 )
 
 var (
+	// Special address that indicates BookinToken payment will be in native coin of
+	// the network (CAM).
 	NativePaymentToken  = common.HexToAddress("0x0000000000000000000000000000000000000000")
 	NativeTokenDecimals = int32(18)
 
+	// Special address that indicates BookinToken payment will occur off-chain.
 	ISOPaymentToken = common.HexToAddress("0x0000000000000000000000000000000000000001")
 	ISODecimals     = int32(6)
 )
