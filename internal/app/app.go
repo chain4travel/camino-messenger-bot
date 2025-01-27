69b4d0c6aa20483d5ee4b2b8f86c86aa7ad40a91
ev.lekht <ev.lekht@gmail.com>
2025-01-27 20:34:04 +0400

    fumpt

--- internal/app/app.go
+++ internal/app/app.go
@@ -150,7 +150,7 @@ func NewApp(ctx context.Context, cfg *config.Config, logger *zap.SugaredLogger) 
 	// get matrix hostname without schema
 	matrixHostname := cfg.Matrix.Host
 	if !strings.Contains(matrixHostname, "://") {
-		// Add dummy protocol to make the url pkg happy, 
+		// Add dummy protocol to make the url pkg happy,
 		// we just want to extract the hostname
 		matrixHostname = "dummy://" + matrixHostname
 	}
