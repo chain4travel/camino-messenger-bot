71d26259e8bf606fa54a7ad138afeb30b9143e2a
Noctunus <alexander.koch@chain4travel.com>
2025-03-05 13:54:33 +0100

    Reactivate other e2e tests.

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -109,8 +109,8 @@ func TestE2E(t *testing.T) {
 		suite.Cleanup,
 	)
 
-	//testsRunner.Register(t, "PingV1 request", tests.TestPingV1)
-	//testsRunner.Register(t, "AccommodationV2", tests.TestAccommodationV2)
+	testsRunner.Register(t, "PingV1 request", tests.TestPingV1)
+	testsRunner.Register(t, "AccommodationV2", tests.TestAccommodationV2)
 	testsRunner.Register(t, "TransportV3", tests.TestTransportV3)
 
 	maxParallelRuns := 0
