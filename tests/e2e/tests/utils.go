474ba4dc3d8e31b722164f89f85d7bf561e43704
Noctunus <alexander.koch@chain4travel.com>
2025-03-06 15:43:11 +0100

    Make the new util functions for debug logging a little bit more robust.

--- tests/e2e/tests/utils.go
+++ tests/e2e/tests/utils.go
@@ -15,6 +15,7 @@ import (
 	"github.com/chain4travel/camino-messenger-bot/pkg/booking"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zapcore"
 	grpcMetadata "google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
@@ -26,12 +27,18 @@ func requestContext(ctx context.Context, metadata *messageMetadata.Metadata) con
 
 // Gets the current function name including the whole package path
 func getCurrentFuncName() string {
-	pc, _, _, _ := runtime.Caller(1)
+	pc, _, _, ok := runtime.Caller(1)
+	if !ok {
+		return "unknown"
+	}
 	return runtime.FuncForPC(pc).Name()
 }
 
 // Get printable type information including the package path
 func getTypeInfo(myvar interface{}) (res string) {
+	if myvar == nil {
+		return "nil"
+	}
 	t := reflect.TypeOf(myvar)
 	for t.Kind() == reflect.Ptr {
 		t = t.Elem()
@@ -42,9 +49,12 @@ func getTypeInfo(myvar interface{}) (res string) {
 
 // Debug print used in each test case to print the request and response as json
 func debugPrintRequestResponse(tt *Test, functionName string, request proto.Message, response proto.Message) {
-	tt.logger.Debugf("Function: %s", functionName)
-	tt.logger.Debugf("Request (%s):\n%s", getTypeInfo(request), protoMessageToJSON(tt, request))
-	tt.logger.Debugf("Response (%s):\n%s", getTypeInfo(response), protoMessageToJSON(tt, response))
+	// Skip the potentially expensive conversion to JSON if debug logging is disabled
+	if tt.logger.Level().Enabled(zapcore.DebugLevel) {
+		tt.logger.Debugf("Function: %s", functionName)
+		tt.logger.Debugf("Request (%s):\n%s", getTypeInfo(request), protoMessageToJSON(tt, request))
+		tt.logger.Debugf("Response (%s):\n%s", getTypeInfo(response), protoMessageToJSON(tt, response))
+	}
 }
 
 // Service function to convert the responses into pretty-printed JSON.
