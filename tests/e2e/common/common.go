6e2dcc98a9cff25994ad7ce6f23caa6fb8061dd0
Noctunus <alexander.koch@chain4travel.com>
2025-04-11 15:56:59 +0200

    Make linter happy

--- tests/e2e/common/common.go
+++ tests/e2e/common/common.go
@@ -9,8 +9,10 @@ import (
 	"github.com/chain4travel/caminogoeth-compat/caminogo/units"
 )
 
-const x2cRate = 1_000_000_000
-const MinBuyableUntilInContract = 1 // seconds -- overrides the bookingtoken default of 1 minute when deployed for the e2e test
+const (
+	x2cRate                   = 1_000_000_000
+	MinBuyableUntilInContract = 1 // seconds -- overrides the bookingtoken default of 1 minute when deployed for the e2e test
+)
 
 var (
 	X2CRateBig                 = big.NewInt(x2cRate)
