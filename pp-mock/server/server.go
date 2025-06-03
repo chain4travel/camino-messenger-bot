13c64f5f0654de6aa23ef78cdc6470bd4a86d74f
ev.lekht <ev.lekht@gmail.com>
2025-06-03 14:23:07 +0400

    cleanup handlers pkgs

--- pp-mock/server/server.go
+++ pp-mock/server/server.go
@@ -32,22 +32,19 @@ import (
 
 	"github.com/chain4travel/camino-messenger-bot/v11/pp-mock/config"
 	"github.com/chain4travel/camino-messenger-bot/v11/pp-mock/events"
-	handlers_accommodation_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/accommodation/v1"
-	handlers_accommodation_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/accommodation/v2"
-	handlers_accommodation_v3 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/accommodation/v3"
-	handlers_mint_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/mint/v1"
-	handlers_mint_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/mint/v2"
-	handlers_mint_v3 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/mint/v3"
-	handlers_validation_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/validation/v1"
-	handlers_validation_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/validation/v2"
-	handlers_validation_v3 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/validation/v3"
-	handlers_cancellation_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/cancellation/v1"
-	handlers_notification_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/notification/v1"
-	handlers_notification_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/notification/v2"
-	handlers_ping_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/ping/v1"
-	handlers_transport_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/transport/v1"
-	handlers_transport_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/transport/v2"
-	handlers_transport_v3 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/transport/v3"
+	accommodation_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/accommodation/v1"
+	accommodation_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/accommodation/v2"
+	accommodation_v3 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/accommodation/v3"
+	book_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/v1"
+	book_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/v2"
+	book_v3 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/book/v3"
+	cancellation_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/cancellation/v1"
+	notification_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/notification/v1"
+	notification_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/notification/v2"
+	ping_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/ping/v1"
+	transport_v1 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/transport/v1"
+	transport_v2 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/transport/v2"
+	transport_v3 "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/handlers/transport/v3"
 	events_pb "github.com/chain4travel/camino-messenger-bot/v11/pp-mock/proto/pb/events"
 )
 
@@ -75,46 +72,46 @@ func Run() error {
 	}
 
 	// Accommodation V1
-	accommodationv1grpc.RegisterAccommodationSearchServiceServer(grpcServer, handlers_accommodation_v1.NewAccommodationSearchV1Server(eventSender))
-	accommodationv1grpc.RegisterAccommodationProductInfoServiceServer(grpcServer, handlers_accommodation_v1.NewAccommodationProductInfoV1Server(eventSender))
-	accommodationv1grpc.RegisterAccommodationProductListServiceServer(grpcServer, handlers_accommodation_v1.NewAccommodationProductListV1Server(eventSender))
+	accommodationv1grpc.RegisterAccommodationSearchServiceServer(grpcServer, accommodation_v1.NewAccommodationSearchV1Server(eventSender))
+	accommodationv1grpc.RegisterAccommodationProductInfoServiceServer(grpcServer, accommodation_v1.NewAccommodationProductInfoV1Server(eventSender))
+	accommodationv1grpc.RegisterAccommodationProductListServiceServer(grpcServer, accommodation_v1.NewAccommodationProductListV1Server(eventSender))
 	// Accommodation V2
-	accommodationv2grpc.RegisterAccommodationSearchServiceServer(grpcServer, handlers_accommodation_v2.NewAccommodationSearchV2Server(eventSender))
-	accommodationv2grpc.RegisterAccommodationProductInfoServiceServer(grpcServer, handlers_accommodation_v2.NewAccommodationProductInfoV2Server(eventSender))
-	accommodationv2grpc.RegisterAccommodationProductListServiceServer(grpcServer, handlers_accommodation_v2.NewAccommodationProductListV2Server(eventSender))
+	accommodationv2grpc.RegisterAccommodationSearchServiceServer(grpcServer, accommodation_v2.NewAccommodationSearchV2Server(eventSender))
+	accommodationv2grpc.RegisterAccommodationProductInfoServiceServer(grpcServer, accommodation_v2.NewAccommodationProductInfoV2Server(eventSender))
+	accommodationv2grpc.RegisterAccommodationProductListServiceServer(grpcServer, accommodation_v2.NewAccommodationProductListV2Server(eventSender))
 	// Accommodation V3
-	accommodationv3grpc.RegisterAccommodationSearchServiceServer(grpcServer, handlers_accommodation_v3.NewAccommodationSearchV3Server(eventSender))
-	accommodationv3grpc.RegisterAccommodationProductInfoServiceServer(grpcServer, handlers_accommodation_v3.NewAccommodationProductInfoV3Server(eventSender))
-	accommodationv3grpc.RegisterAccommodationProductListServiceServer(grpcServer, handlers_accommodation_v3.NewAccommodationProductListV3Server(eventSender))
+	accommodationv3grpc.RegisterAccommodationSearchServiceServer(grpcServer, accommodation_v3.NewAccommodationSearchV3Server(eventSender))
+	accommodationv3grpc.RegisterAccommodationProductInfoServiceServer(grpcServer, accommodation_v3.NewAccommodationProductInfoV3Server(eventSender))
+	accommodationv3grpc.RegisterAccommodationProductListServiceServer(grpcServer, accommodation_v3.NewAccommodationProductListV3Server(eventSender))
 
 	// Book V1
-	bookv1grpc.RegisterMintServiceServer(grpcServer, handlers_mint_v1.NewMintServiceV1Server(eventSender))
-	bookv1grpc.RegisterValidationServiceServer(grpcServer, handlers_validation_v1.NewValidationServiceV1Server(eventSender))
+	bookv1grpc.RegisterMintServiceServer(grpcServer, book_v1.NewMintServiceV1Server(eventSender))
+	bookv1grpc.RegisterValidationServiceServer(grpcServer, book_v1.NewValidationServiceV1Server(eventSender))
 	// Book V2
-	bookv2grpc.RegisterMintServiceServer(grpcServer, handlers_mint_v2.NewMintServiceV2Server(eventSender))
-	bookv2grpc.RegisterValidationServiceServer(grpcServer, handlers_validation_v2.NewValidationServiceV2Server(eventSender))
+	bookv2grpc.RegisterMintServiceServer(grpcServer, book_v2.NewMintServiceV2Server(eventSender))
+	bookv2grpc.RegisterValidationServiceServer(grpcServer, book_v2.NewValidationServiceV2Server(eventSender))
 	// Book V3
-	bookv3grpc.RegisterMintServiceServer(grpcServer, handlers_mint_v3.NewMintServiceV3Server(eventSender))
-	bookv3grpc.RegisterValidationServiceServer(grpcServer, handlers_validation_v3.NewValidationServiceV3Server(eventSender))
+	bookv3grpc.RegisterMintServiceServer(grpcServer, book_v3.NewMintServiceV3Server(eventSender))
+	bookv3grpc.RegisterValidationServiceServer(grpcServer, book_v3.NewValidationServiceV3Server(eventSender))
 
 	// Ping V1
-	pingv1grpc.RegisterPingServiceServer(grpcServer, handlers_ping_v1.NewPingServiceV1Server(eventSender))
+	pingv1grpc.RegisterPingServiceServer(grpcServer, ping_v1.NewPingServiceV1Server(eventSender))
 
 	// Notification V1
-	notificationv1grpc.RegisterNotificationServiceServer(grpcServer, handlers_notification_v1.NewNotificationServiceV1Server(eventSender))
+	notificationv1grpc.RegisterNotificationServiceServer(grpcServer, notification_v1.NewNotificationServiceV1Server(eventSender))
 	// Notification V2
-	notificationv2grpc.RegisterNotificationServiceServer(grpcServer, handlers_notification_v2.NewNotificationServiceV2Server(eventSender))
+	notificationv2grpc.RegisterNotificationServiceServer(grpcServer, notification_v2.NewNotificationServiceV2Server(eventSender))
 
 	// Transport V1
-	transportv1grpc.RegisterTransportSearchServiceServer(grpcServer, handlers_transport_v1.NewTransportSearchV1Server(eventSender))
+	transportv1grpc.RegisterTransportSearchServiceServer(grpcServer, transport_v1.NewTransportSearchV1Server(eventSender))
 	// Transport V2
-	transportv2grpc.RegisterTransportSearchServiceServer(grpcServer, handlers_transport_v2.NewTransportSearchV2Server(eventSender))
+	transportv2grpc.RegisterTransportSearchServiceServer(grpcServer, transport_v2.NewTransportSearchV2Server(eventSender))
 	// Transport V3
-	transportv3grpc.RegisterTransportProductListServiceServer(grpcServer, handlers_transport_v3.NewTransportProductListV3Server(eventSender))
-	transportv3grpc.RegisterTransportSearchServiceServer(grpcServer, handlers_transport_v3.NewTransportSearchV3Server(eventSender))
+	transportv3grpc.RegisterTransportProductListServiceServer(grpcServer, transport_v3.NewTransportProductListV3Server(eventSender))
+	transportv3grpc.RegisterTransportSearchServiceServer(grpcServer, transport_v3.NewTransportSearchV3Server(eventSender))
 
 	// Cancellation V1
-	cancellationv1grpc.RegisterCheckCancellationServiceServer(grpcServer, handlers_cancellation_v1.NewCheckCancellationV1Server(eventSender))
+	cancellationv1grpc.RegisterCheckCancellationServiceServer(grpcServer, cancellation_v1.NewCheckCancellationV1Server(eventSender))
 
 	reflection.Register(grpcServer)
 
