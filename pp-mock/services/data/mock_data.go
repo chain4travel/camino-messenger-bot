b89c3de62b49be78999388986d35a1534ad8f773
ev.lekht <ev.lekht@gmail.com>
2025-08-05 21:09:19 +0400

    wip

--- pp-mock/services/data/mock_data.go
+++ pp-mock/services/data/mock_data.go
@@ -103,6 +103,10 @@ var (
 )
 
 func init() {
+	// because protobuf location and price are one-of interface types,
+	// json unmarshaling won't work for them and will result in error
+	// so, as quick workaround, we are setting them manually
+
 	if err := json.Unmarshal(propertiesJSON, &PropertiesV1); err != nil {
 		panic(fmt.Errorf("error unmarshaling properties v1: %w", err))
 	}
@@ -154,13 +158,173 @@ func init() {
 	// if err := json.Unmarshal(seatMapV3JSON, &SeatMapV3); err != nil {
 	// 	panic(fmt.Errorf("error unmarshaling seat map v3: %w", err))
 	// }
-	// if err := json.Unmarshal(seatMapAvailabilityV3JSON, &SeatMapAvailabilityV3); err != nil {
-	// 	panic(fmt.Errorf("error unmarshaling seat map availability v3: %w", err))
-	// }
-
-	// because protobuf location and price are one-of interface types,
-	// json unmarshaling won't work for them and will result in error
-	// so, as quick workaround, we are setting them manually
+	if err := unmarshalStrictAndValidate(seatMapAvailabilityV4JSON, &SeatMapAvailabilityV4, func(seatMapAvailabilityV4 []*typesv4.SeatMapInventory) {
+		seatMapAvailabilityV4[0].Sections[0].SeatInfo = &typesv4.SectionInventory_SeatList{
+			SeatList: &typesv4.SeatInventory{
+				Ids: []string{"1A", "1C", "1D", "1F"},
+			},
+		}
+		seatMapAvailabilityV4[0].Sections[1].SeatInfo = &typesv4.SectionInventory_SeatList{
+			SeatList: &typesv4.SeatInventory{
+				Ids: []string{"2A", "2C", "2D", "2F"},
+			},
+		}
+		seatMapAvailabilityV4[0].Sections[2].SeatInfo = &typesv4.SectionInventory_SeatList{
+			SeatList: &typesv4.SeatInventory{
+				Ids: []string{"4D", "6A", "6C", "9F", "11E", "14A", "16F", "17B", "19C", "23A", "26E", "28C", "30D", "31F", "34B", "36E", "37F", "37A", "38B", "38E"},
+			},
+		}
+		seatMapAvailabilityV4[0].Sections[3].SeatInfo = &typesv4.SectionInventory_SeatList{
+			SeatList: &typesv4.SeatInventory{
+				Ids: []string{
+					"3A", "3B", "3C", "3D", "3E", "3F",
+					"4A", "4B", "4C", "4E", "4F",
+					"5A", "5B", "5C", "5D", "5E", "5F",
+					"6B", "6D", "6E", "6F",
+					"7A", "7B", "7C", "7D", "7E", "7F",
+					"8A", "8B", "8C", "8D", "8E", "8F",
+					"9A", "9B", "9C", "9D", "9E",
+					"10A", "10B", "10C", "10D", "10E", "10F",
+					"11A", "11B", "11C", "11D", "11F",
+					"12A", "12B", "12C", "12D", "12E", "12F",
+					"13A", "13B", "13C", "13D", "13E", "13F",
+					"14B", "14C", "14D", "14E", "14F",
+					// "15A",
+					// "15B",
+					// "15C",
+					// "15D",
+					// "15E",
+					// "15F",
+					// "16A",
+					// "16B",
+					// "16C",
+					// "16D",
+					// "16E",
+					// "17A",
+					// "17C",
+					// "17D",
+					// "17E",
+					// "17F",
+					// "18A",
+					// "18B",
+					// "18C",
+					// "18D",
+					// "18E",
+					// "18F",
+					// "19A",
+					// "19B",
+					// "19D",
+					// "19E",
+					// "19F",
+					// "20A",
+					// "20B",
+					// "20C",
+					// "20D",
+					// "20E",
+					// "20F",
+					// "21A",
+					// "21B",
+					// "21C",
+					// "21D",
+					// "21E",
+					// "21F",
+					// "22A",
+					// "22B",
+					// "22C",
+					// "22D",
+					// "22E",
+					// "22F",
+					// "23B",
+					// "23C",
+					// "23D",
+					// "23E",
+					// "23F",
+					// "24A",
+					// "24B",
+					// "24C",
+					// "24D",
+					// "24E",
+					// "24F",
+					// "25A",
+					// "25B",
+					// "25C",
+					// "25D",
+					// "25E",
+					// "25F",
+					// "26A",
+					// "26B",
+					// "26C",
+					// "26D",
+					// "26F",
+					// "27A",
+					// "27B",
+					// "27C",
+					// "27D",
+					// "27E",
+					// "27F",
+					// "28A",
+					// "28B",
+					// "28D",
+					// "28E",
+					// "28F",
+					// "29A",
+					// "29B",
+					// "29C",
+					// "29D",
+					// "29E",
+					// "29F",
+					// "30A",
+					// "30B",
+					// "30C",
+					// "30E",
+					// "30F",
+					// "31A",
+					// "31B",
+					// "31C",
+					// "31D",
+					// "31E",
+					// "32A",
+					// "32B",
+					// "32C",
+					// "32D",
+					// "32E",
+					// "32F",
+					// "33A",
+					// "33B",
+					// "33C",
+					// "33D",
+					// "33E",
+					// "33F",
+					// "34A",
+					// "34C",
+					// "34D",
+					// "34E",
+					// "34F",
+					// "35A",
+					// "35B",
+					// "35C",
+					// "35D",
+					// "35E",
+					// "35F",
+					// "36A",
+					// "36B",
+					// "36C",
+					// "36D",
+					// "36F",
+					// "37B",
+					// "37C",
+					// "37D",
+					// "37E",
+					// "38A",
+					// "38C",
+					// "38D",
+					// "38F"
+				},
+			},
+		}
+	}); err != nil {
+		panic(fmt.Errorf("error unmarshaling seat map availability v4: %w", err))
+	}
 
 	// TripBasicV3[0,0]
 	TripsBasicV3[0].Segments[0].Departure.Location = &transportv3.TransitEventLocation{
@@ -408,7 +572,7 @@ func init() {
 	// TODO @evlekht do all data checks like make sure that properties has prop.Property.ContactInfo.Address[0] != nil
 }
 
-func unmarshalStrictAndValidate[T proto.Message](data []byte, destination *[]T, postUnmarshal func([]T)) error { //nolint:unused
+func unmarshalStrictAndValidate[T proto.Message](data []byte, destination *[]T, postUnmarshal func([]T)) error {
 	decoder := json.NewDecoder(bytes.NewReader(data))
 	decoder.DisallowUnknownFields()
 	if err := decoder.Decode(destination); err != nil {
