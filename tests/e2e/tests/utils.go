01961890fb0e9d49d7ecd55cc5c520a99a28bb91
ev.lekht <ev.lekht@gmail.com>
2025-10-22 16:50:42 +0400

    wip

--- tests/e2e/tests/utils.go
+++ tests/e2e/tests/utils.go
@@ -43,13 +43,13 @@ func requestContext(ctx context.Context, recipientCMAccount common.Address) cont
 	))
 }
 
-func priceBigV4(t *testing.T, value string, decimals int32, currency *typesv4.Currency) *big.Int {
+func priceBigV4(t *testing.T, value string, decimals int32, currency *typesv4.Currency) *big.Int { //nolint:unused // will be used in following PRs
 	priceBig, err := price.ToBigInt(value, decimals, currencyDecimalsV4(t, currency))
 	require.NoError(t, err)
 	return priceBig
 }
 
-func protoPriceBigV4(t *testing.T, protoPrice *typesv4.Price) *big.Int {
+func protoPriceBigV4(t *testing.T, protoPrice *typesv4.Price) *big.Int { //nolint:unused // will be used in following PRs
 	priceBig, err := price.ToBigInt(protoPrice.Value, conversion.MustUInt32ToInt32(protoPrice.Decimals), currencyDecimalsV4(t, protoPrice.Currency))
 	require.NoError(t, err)
 	return priceBig
@@ -101,7 +101,7 @@ func protoPriceBigV2(t *testing.T, protoPrice *typesv2.Price) *big.Int {
 	return priceBig
 }
 
-func getPaymentTokenFromPriceV4(t *testing.T, price *typesv4.Price) common.Address {
+func getPaymentTokenFromPriceV4(t *testing.T, price *typesv4.Price) common.Address { //nolint:unused // will be used in following PRs
 	require.NotNil(t, price, "unexpected nil price")
 	switch currency := price.GetCurrency().GetCurrency().(type) {
 	case *typesv4.Currency_NativeToken:
@@ -140,7 +140,7 @@ func calculateCashIn(value *big.Int) (cashedIn *big.Int, c4tFeeCut *big.Int) { /
 	return big.NewInt(0).Sub(value, c4tFeeCut), c4tFeeCut
 }
 
-func requireProtoSlicesElementsMatch[T proto.Message](t *testing.T, expected, actual []T) {
+func requireProtoSlicesElementsMatch[T proto.Message](t *testing.T, expected, actual []T) { //nolint:unused // will be used in following PRs
 	protoMarshal := proto.MarshalOptions{Deterministic: true}
 	opts := []cmp.Option{
 		cmpopts.SortSlices(func(x, y T) bool {
