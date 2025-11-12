97b15ff297c4da819fddb04ab08fa9a75397ceaa
ev.lekht <ev.lekht@gmail.com>
2025-11-12 19:56:24 +0400

    cleanup

--- pp-mock/handlers/ping/v1/ping_handler.go
+++ pp-mock/handlers/ping/v1/ping_handler.go
@@ -9,7 +9,6 @@ import (
 
 	"buf.build/gen/go/chain4travel/camino-messenger-protocol/grpc/go/cmp/services/ping/v1/pingv1grpc"
 	pingv1 "buf.build/gen/go/chain4travel/camino-messenger-protocol/protocolbuffers/go/cmp/services/ping/v1"
-	typesv1 "buf.build/gen/go/chain4travel/camino-messenger-protocol/protocolbuffers/go/cmp/types/v1"
 	"github.com/chain4travel/camino-messenger-bot/v12/pp-mock/common"
 	"github.com/chain4travel/camino-messenger-bot/v12/pp-mock/metadata"
 )
