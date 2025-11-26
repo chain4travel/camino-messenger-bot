7f92d219339f4efaa537d6f748d5e28949b90146
ev.lekht <ev.lekht@gmail.com>
2025-11-27 01:00:25 +0400

    wip

--- pkg/chequehandler/storage/sqlite/migrations/2_payment_token.up.sql
+++ pkg/chequehandler/storage/sqlite/migrations/2_payment_token.up.sql
@@ -1,2 +1,5 @@
 ALTER TABLE cheque_records
 ADD payment_token VARBINARY(20) NOT NULL DEFAULT X'0000000000000000000000000000000000000000';
+
+<-- TODO@ also fix bot logic to ignore 0x00 cheques, since for now we can only cash in erc-20
+// TODO@ add removed attributes in activities back
