a181578d92f362282c307204e5994e621d1adb26
ev.lekht <ev.lekht@gmail.com>
2025-12-01 19:00:28 +0400

    wip

--- pp-mock/services/data/accommodation/servicev4.go
+++ pp-mock/services/data/accommodation/servicev4.go
@@ -7,66 +7,41 @@ import (
 	"fmt"
 
 	typesv4 "buf.build/gen/go/chain4travel/camino-messenger-protocol/protocolbuffers/go/cmp/types/v4"
-	"github.com/chain4travel/camino-messenger-bot/v12/pp-mock/common"
 )
 
-type ServiceV4 struct {
-	Fact           *typesv4.ServiceFact
-	FactDefinition *typesv4.ServiceFactDefinition
+func VerifyAndGetMapping(facts []*typesv4.ServiceFact, factDefinitions []*typesv4.ServiceFactDefinition) map[string]*typesv4.ServiceFactDefinition {
+	h := &helper{
+		definitions:     make(map[string]*typesv4.ServiceFactDefinition),
+		factDefinitions: factDefinitions,
+	}
+
+	for _, fact := range facts {
+		h.setFactDefinition(fact)
+	}
+
+	if len(h.definitions) != h.expectedDefinitionsLen {
+		panic("mock data error: some service facts have no matching fact definitions")
+	}
+
+	return h.definitions
 }
 
-func (t *ServiceV4) Clone() *ServiceV4 {
-	return &ServiceV4{
-		Fact:           common.CloneProto(t.Fact),
-		FactDefinition: common.CloneProto(t.FactDefinition),
-	}
+type helper struct {
+	expectedDefinitionsLen int
+	definitions            map[string]*typesv4.ServiceFactDefinition
+	factDefinitions        []*typesv4.ServiceFactDefinition
 }
 
-func (t *ServiceV4) Verify() error {
-	if t.Fact.Code != t.FactDefinition.Code {
-		return fmt.Errorf("trip basic and extended supplier code mismatch")
-	}
-	return nil
-}
-
-func VerifyAndGetServices(facts []*typesv4.ServiceFact, factDefinitions []*typesv4.ServiceFactDefinition) []*ServiceV4 {
-	if len(facts) != len(factDefinitions) {
-		panic(fmt.Errorf("mock data error: number of serviceFact (%d) does not match number of serviceFactDefinition (%d)", len(facts), len(factDefinitions)))
-	}
-	services := make([]*ServiceV4, 0, len(facts))
-	for i, fact := range facts {
-		service := &ServiceV4{
-			Fact:           fact,
-			FactDefinition: factDefinitions[i],
+func (h *helper) setFactDefinition(fact *typesv4.ServiceFact) {
+	h.expectedDefinitionsLen++
+	for _, factDefinition := range h.factDefinitions {
+		if fact.Code == factDefinition.Code {
+			h.definitions[fact.Code] = factDefinition
+			for _, subFact := range fact.Details {
+				h.setFactDefinition(subFact)
+			}
+			return
 		}
-		if err := service.Verify(); err != nil {
-			panic(fmt.Errorf("mock data error: service fact/fact definition at index %d are invalid: %w", i, err))
-		}
-		services = append(services, service)
 	}
-	return services
-}
-
-func CloneV4(services []*ServiceV4) []*ServiceV4 {
-	cloned := make([]*ServiceV4, len(services))
-	for i, service := range services {
-		cloned[i] = service.Clone()
-	}
-	return cloned
-}
-
-func FactV4(services []*ServiceV4) []*typesv4.ServiceFact {
-	facts := make([]*typesv4.ServiceFact, len(services))
-	for i, service := range services {
-		facts[i] = common.CloneProto(service.Fact)
-	}
-	return facts
-}
-
-func FactDefinitionV4(services []*ServiceV4) []*typesv4.ServiceFactDefinition {
-	factDefinitions := make([]*typesv4.ServiceFactDefinition, len(services))
-	for i, service := range services {
-		factDefinitions[i] = common.CloneProto(service.FactDefinition)
-	}
-	return factDefinitions
+	panic(fmt.Sprintf("mock data error: service fact code %s has no matching fact definition", fact.Code))
 }
--- pp-mock/services/data/mock_data.go
+++ pp-mock/services/data/mock_data.go
@@ -95,12 +95,12 @@ var seatMapV4JSON []byte
 var seatMapAvailabilityV4JSON []byte
 
 var (
-	PropertiesV2             []*accommodationv2.PropertyExtendedInfo // used by product list, info and search
-	PropertiesV3             []*accommodationv3.PropertyExtendedInfo // used by product list, info and search
-	PropertiesV4             []*accommodationv4.PropertyExtendedInfo // used by product list, info and search
-	ServiceFactsV4           []*typesv4.ServiceFact                  // TODO@
-	ServiceFactDefinitionsV4 []*typesv4.ServiceFactDefinition        // TODO@
-	ServicesV4               []*accommodation.ServiceV4              // TODO@
+	PropertiesV2                    []*accommodationv2.PropertyExtendedInfo   // used by product list, info and search
+	PropertiesV3                    []*accommodationv3.PropertyExtendedInfo   // used by product list, info and search
+	PropertiesV4                    []*accommodationv4.PropertyExtendedInfo   // used by product list, info and search
+	ServiceFactsV4                  []*typesv4.ServiceFact                    // TODO@
+	ServiceFactDefinitionsV4        []*typesv4.ServiceFactDefinition          // TODO@
+	ServiceFactDefinitionsV4Mapping map[string]*typesv4.ServiceFactDefinition // TODO@
 
 	TripsV2 []*transportv2.Trip // used by search
 
@@ -135,7 +135,7 @@ func init() {
 	PropertiesV4 = mustUnmarshalStrictAndValidate[*accommodationv4.PropertyExtendedInfo](propertiesV4JSON, "error unmarshaling properties v4")
 	ServiceFactsV4 = mustUnmarshalStrictAndValidate[*typesv4.ServiceFact](serviceFactsV4JSON, "error unmarshaling service facts v4")
 	ServiceFactDefinitionsV4 = mustUnmarshalStrictAndValidate[*typesv4.ServiceFactDefinition](serviceFactDefinitionsV4JSON, "error unmarshaling service fact definitions v4")
-	ServicesV4 = accommodation.VerifyAndGetServices(ServiceFactsV4, ServiceFactDefinitionsV4)
+	ServiceFactDefinitionsV4Mapping = accommodation.VerifyAndGetMapping(ServiceFactsV4, ServiceFactDefinitionsV4)
 	// TransportV2
 	TripsV2 = mustUnmarshalStrictAndValidate[*transportv2.Trip](tripsV2JSON, "error unmarshaling trips v2")
 	// TransportV3
