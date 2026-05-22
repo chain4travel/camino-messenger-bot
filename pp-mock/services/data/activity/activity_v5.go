43aae95e2feadd51d2261803f9d0dcbf260b52f2
Ekrem Seren <ekrem.seren@gmail.com>
2026-05-22 16:01:15 +0300

    refactor(activity/v2): drop obsolete v2 activity generated files

---
+++ pp-mock/services/data/activity/activity_v5.go
@@ -0,0 +1,48 @@
+// Copyright (C) 2022-2026, Chain4Travel AG. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package activity
+
+import (
+	"fmt"
+	"slices"
+
+	activityv5 "buf.build/gen/go/chain4travel/camino-messenger-protocol/protocolbuffers/go/cmp/services/activity/v5"
+	"google.golang.org/protobuf/proto"
+)
+
+func VerifyV5(extended []*activityv5.ActivityExtendedInfo, searchResults []*activityv5.ActivitySearchResult) {
+	expectedCount := 0
+	for _, activity := range extended {
+		expectedCount += len(activity.Units) * len(activity.Services)
+	}
+	if len(searchResults) != expectedCount {
+		panic(fmt.Errorf("mock data error: expected %d search results but got %d", expectedCount, len(searchResults)))
+	}
+
+	i := 0
+	for _, activity := range extended {
+		for _, unit := range activity.Units {
+			for _, service := range activity.Services {
+				searchResult := searchResults[i]
+				if !proto.Equal(activity.Activity.SupplierCode, searchResult.SupplierCode) {
+					panic("activityExtendedInfo and searchResult supplier code mismatch")
+				}
+				if unit.Code != searchResult.UnitCode {
+					panic("activityExtendedInfo and searchResult unit code mismatch")
+				}
+				if service.Code != searchResult.ServiceCode {
+					panic("activityExtendedInfo and searchResult service code mismatch")
+				}
+				for _, searchResultZoneCode := range searchResult.ZoneCodes {
+					if !slices.ContainsFunc(activity.Zones, func(z *activityv5.TransferZone) bool {
+						return z.Code == searchResultZoneCode
+					}) {
+						panic("activityExtendedInfo and searchResult zone code mismatch")
+					}
+				}
+				i++
+			}
+		}
+	}
+}
